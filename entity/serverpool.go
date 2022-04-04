27ce5aa3382d5409c11e2c8d0134dafa276d76c1
geraldofigueiredo <geraldo.figueiredosj@gmail.com>
2022-04-03 22:35:54 -0300

    add backend count/index serverPool

--- entity/serverpool.go
+++ entity/serverpool.go
@@ -3,15 +3,33 @@ package entity
 type ServerPool struct {
 	Backends []*Backend
 	Current  uint64
+	count    int
 }
 
+// NewEmptyServerPool creates a new ServerPool
+// count == -1 means empty pool
 func NewEmptyServerPool() *ServerPool {
 	return &ServerPool{
 		Backends: make([]*Backend, 0),
 		Current:  0,
+		count:    -1,
 	}
 }
 
-func (s *ServerPool) AddBackend(backend *Backend) {
+func (s *ServerPool) AddBackend(backend *Backend) (index int) {
 	s.Backends = append(s.Backends, backend)
+	s.count++
+	return s.count
+}
+
+func (s *ServerPool) GetPoolSize() int {
+	return s.count
+}
+
+func (s *ServerPool) SetBackendAlive(index int, alive bool) {
+	s.Backends[index].SetAlive(alive)
+}
+
+func (s *ServerPool) GetBackend(index int) *Backend {
+	return s.Backends[index]
 }
