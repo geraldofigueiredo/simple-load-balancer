eab8f5430b5e80d804260f1534b42aa067b96cc3
geraldofigueiredo <geraldo.figueiredosj@gmail.com>
2022-04-03 22:38:20 -0300

    methods to get infos from context

--- server/server.go
+++ server/server.go
@@ -68,3 +68,19 @@ func InitServer() {
 		Handler: http.HandlerFunc(lbController.LB),
 	}
 }
+
+// GetAttemptsFromContext returns the attempts for request
+func GetAttemptsFromContext(r *http.Request) int {
+	if attempts, ok := r.Context().Value(Attempts).(int); ok {
+		return attempts
+	}
+	return 1
+}
+
+// GetRetryFromContext returns the attempts for request
+func GetRetryFromContext(r *http.Request) int {
+	if retry, ok := r.Context().Value(Retry).(int); ok {
+		return retry
+	}
+	return 0
+}
